1b56d55d6a4fe4b2578019fb8f28b4a413cff8c6
toshihisa handa <toshihisa.handa@caters.co.jp>
2021-02-03 12:00:55 +0900

    画像アップロード完了。次はデータ取得と表示

--- view/drege.php
+++ view/drege.php
@@ -1,4 +1,78 @@
-<?php include('favicon.php') ?>
+<?php 
+include('../common/funcs.php');
+$title = $_POST['title'];
+$tag = $_POST['tag'];
+$text = $_POST['text'];
+
+
+//DB接続
+try{
+  $pdo = new PDO('mysql:host=localhost;dbname=lf', 'root', 'root');
+} catch(PDOException $e) {
+  //ここでエラー時の内容を確認できるようになる。これがないとerror500が出るだけ
+  print "エラー！" . $e->getMessage() . "<br/>";
+  die('終了します');
+}
+
+//画像処理
+if($_SERVER['REQUEST_METHOD'] != 'POST'){
+  //画像を取得
+    
+    //2．データ登録SQL作成
+  //prepare("")の中にはmysqlのSQLで入力したINSERT文を入れて修正すれば良いイメージ
+  $stmt = $pdo->prepare("SELECT* FROM diary ORDER BY created_at DESC");//日付で登録が新しいものが上になる様に抽出
+  $status = $stmt->execute();
+  $images = $stmt->fetchAll();//今までなかった記述。画像のアップロード特有
+  
+  
+  
+  }else{
+    //POSTデータ取得
+    //ここのFILESで[]されているimageはinput type=fileタグのname部分の名称
+    $imgname = date("Ymd") . random_int(1, 999999) . $_FILES['image']['name'];//ここのnameはアップロードされたファイルのファイル名
+
+  
+    //指定フォルダに画像を保存
+    $save = '../upload/' . basename($imgname);//保存先作成://ファイル名を使用して保存先ディレクトリを指定 basename()でファイルシステムトラバーサル攻撃を防ぐ
+    move_uploaded_file($_FILES['image']['tmp_name'], $save);//指定した保存先へ保存
+  
+  //３．データ登録SQL作成
+  //prepare("")の中にはmysqlのSQLで入力したINSERT文を入れて修正すれば良いイメージ
+  $stmt = $pdo->prepare("INSERT INTO diary(title,image,tag,text,created_at,user_id)VALUES(:title, :imgname,:tag,:text,sysdate(),1);
+  ");
+  $stmt->bindValue(':title', $title, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)  第３引数は省略出来るが、セキュリティの観点から記述している。文字列か数値はmysqlのデータベースに登録したものがvarcharaかintかというところで判断する
+  $stmt->bindValue(':imgname', $imgname, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)  第３引数は省略出来るが、セキュリティの観点から記述している。文字列か数値はmysqlのデータベースに登録したものがvarcharaかintかというところで判断する
+  $stmt->bindValue(':tag', $tag, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+  $stmt->bindValue(':text', $text, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+  $status = $stmt->execute();
+  
+  
+  //４．データ登録処理後（基本コピペ使用でOK)
+  if($status==false){
+    //SQL実行時にエラーがある場合（エラーオブジェクト取得して表示）
+    $error = $stmt->errorInfo();
+    exit("SQLError:".$error[2]);//エラーが起きたらエラーの2番目の配列から取ります。ここは考えず、これを使えばOK
+                               // SQLEErrorの部分はエラー時出てくる文なのでなんでもOK
+  }else{
+    //５．index.phpへリダイレクト(エラーがなければindex.phpt)
+    header('Location: drege.php');//Location:の後ろの半角スペースは必ず入れる。
+    exit();
+  
+  }
+    
+  }
+
+
+
+
+
+?>
+
+
+
+
+
+<?php include('../common/favicon.php') ?>
 
     <title>日記登録</title>
     <?php include('../common/style.php') ?>
@@ -39,7 +113,7 @@
 
      <div class="main">
         <h1>日記登録</h1>
-        <form action="/d_insert" method="POST" enctype="multipart/form-data">
+        <form  method="POST" enctype="multipart/form-data">
             <div id='attachment'>
                 <label>
                     <input type="file" name="image" class="fileinput">日記の登録
