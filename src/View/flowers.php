84fe91fd8a804c68618da6907c0a78da3aff9f81
toshihisa handa <toshihisa.handa@caters.co.jp>
2021-02-10 19:56:55 +0900

    myprofiele.phpを作ろうとして会員登録時に店舗登録をする処理を忘れていたことに気づく。

--- src/View/flowers.php
+++ src/View/flowers.php
@@ -1,7 +1,6 @@
-<?php 
+<?php
 session_start();
-include('../../common/favicon.html') 
-?>
+include('../../common/funcs.php');
 $name = $_POST['name'];
 $price = $_POST['price'];
 $feature = $_POST['feature'];
@@ -11,23 +10,17 @@ $text = $_POST['text'];
 
 
 //DB接続
-try{
-  $pdo = new PDO('mysql:host=localhost;dbname=lf', 'root', 'root');
-} catch(PDOException $e) {
-  //ここでエラー時の内容を確認できるようになる。これがないとerror500が出るだけ
-  print "エラー！" . $e->getMessage() . "<br/>";
-  die('終了します');
-}
+$pdo = dbcon();
 
 
-    //2．データ登録SQL作成
-  //prepare("")の中にはmysqlのSQLで入力したINSERT文を入れて修正すれば良いイメージ
-  $stmt = $pdo->prepare("SELECT* FROM flower ORDER BY created_at DESC");//日付で登録が新しいものが上になる様に抽出
-  $status = $stmt->execute();
-  $result = $stmt->fetchAll();//今までなかった記述。画像のアップロード特有
-  
-  
-  
+//2．データ登録SQL作成
+//prepare("")の中にはmysqlのSQLで入力したINSERT文を入れて修正すれば良いイメージ
+$stmt = $pdo->prepare("SELECT* FROM flower ORDER BY created_at DESC"); //日付で登録が新しいものが上になる様に抽出
+$status = $stmt->execute();
+$result = $stmt->fetchAll(); //今までなかった記述。画像のアップロード特有
+
+
+
 
 
 
@@ -40,29 +33,29 @@ try{
 
 
 
-<?php include('favicon.html') ?>
+<?php include('../../common/favicon.html'); ?>
+<title>花一覧</title>
+<?php include('../../common/style.html') ?>
+<link rel="stylesheet" href="/public/css/flowers.css">
 
-    <title>花一覧</title>
-    <?php include('../../common/style.html') ?>
-    <link rel="stylesheet" href="/public/css/flowers.css">
 
- 
 </head>
+
 <body>
 
 
 
-<div class="flowers-glid">
+  <div class="flowers-glid">
     <header>
-        <ul>
+      <ul>
         <?php include('../../common/header-nav-leftIcon.html') ?>
 
-          <div class='nav-right'>
-            <li class='searchNav'>
-                <form action="/f_search" method='get'>
-                <span class='search-bar'>Search</span><input  class='search t-search' type="text" name='kensaku' placeholder="検索ワード入力" required>
+        <div class='nav-right'>
+          <li class='searchNav'>
+            <form action="/f_search" method='get'>
+              <span class='search-bar'>Search</span><input class='search t-search' type="text" name='kensaku' placeholder="検索ワード入力" required>
             </form>
-           </li>
+          </li>
           <!-- <% if (typeof user == 'undefined') { %>
           <li class='log'><a href="/login" class='hlink'>Login</a></li>
           <% } else{%>
@@ -79,76 +72,76 @@ try{
                   <% } %>
             </a>
         </li> -->
-     </div>
- 
-        </ul>
-     </header>
-     <div class="img1">
-        <img src="/public/images/flowerimg2.png" alt="">
+        </div>
+
+      </ul>
+    </header>
+    <div class="img1">
+      <img src="/public/images/flowerimg2.png" alt="">
     </div>
-     <div class="title1">
-        <h1 class='topTitle'>Flower List</h1>
+    <div class="title1">
+      <h1 class='topTitle'>Flower List</h1>
     </div>
-     
-</div>
 
- 
-
- 
-
-
-<div class="flower-list">
-    <div class="flowerList">
-       
-        <div class="flower-container">
-        <?php for($i = 0; $i <count($result); $i++): ?>
-             <div class="fcard">
-                <div class='flower-card'>
-                    <a href="flower.php/<?= $result[$i]['id']; ?>">
-                    <img src="/public/upload/<?= $result[$i]['image']; ?>" alt="">
-                    <h3 class='fsname'><?= $result[$i]['name']; ?></h3>
-                    <div class='fprice'><?= $result[$i]['price']; ?>円（税込）</div>
-                    <div class='ffeature'><?= $result[$i]['feature']; ?></div>
-                </a>
-               </div>
-               <!-- <h2 class='fname'><?= $result[$i]['shopname']; ?></h2> -->
-            </div>
-            <?php endfor; ?>
-            </div>
   </div>
-</div>
 
 
-<!-- フッター ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝-->
-<div class="footer-glid">
+
+
+
+
+  <div class="flower-list">
+    <div class="flowerList">
+
+      <div class="flower-container">
+        <?php for ($i = 0; $i < count($result); $i++) : ?>
+          <div class="fcard">
+            <div class='flower-card'>
+              <a href="flower.php/<?= $result[$i]['id']; ?>">
+                <img src="/public/upload/<?= $result[$i]['image']; ?>" alt="">
+                <h3 class='fsname'><?= $result[$i]['name']; ?></h3>
+                <div class='fprice'><?= $result[$i]['price']; ?>円（税込）</div>
+                <div class='ffeature'><?= $result[$i]['feature']; ?></div>
+              </a>
+            </div>
+            <!-- <h2 class='fname'><?= $result[$i]['shopname']; ?></h2> -->
+          </div>
+        <?php endfor; ?>
+      </div>
+    </div>
+  </div>
+
+
+  <!-- フッター ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝-->
+  <div class="footer-glid">
     <footer>
-        <h3 class='topSubtitle'>Copyright  second-cube</h3>
+      <h3 class='topSubtitle'>Copyright second-cube</h3>
     </footer>
 
     <!-- フッターナビ -->
-    <div class='Fnav web ' >
-    <ul class='navs'>
-    <li ><a href="/map" ><img class='navimg' src="/public/images/map-24.png" alt=""></a><a href="/map" class='Fnav-link hlink'>Map</a></li>
-    <li ><a href="/shops" ><img class='navimg' src="/public/images/shop-24.png" alt=""></a><a href="/shops" class='Fnav-link hlink'>Shop</a></li>
-    <li ><a href="/diarys" ><img class='navimg' src="/public/images/script-24.png" alt=""></a><a href="/diarys" class='Fnav-link hlink'>Diary</a></li>
-    <li ><a href="/flowers" ><img class='navimg' src="/public/images/flower-24.png" alt=""></a><a href="/flowers" class='Fnav-link hlink'>Flower</a></li>
-    <!-- <li><div id='search'>検索</div></li> -->
+    <div class='Fnav web '>
+      <ul class='navs'>
+        <li><a href="/map"><img class='navimg' src="/public/images/map-24.png" alt=""></a><a href="/map" class='Fnav-link hlink'>Map</a></li>
+        <li><a href="/shops"><img class='navimg' src="/public/images/shop-24.png" alt=""></a><a href="/shops" class='Fnav-link hlink'>Shop</a></li>
+        <li><a href="/diarys"><img class='navimg' src="/public/images/script-24.png" alt=""></a><a href="/diarys" class='Fnav-link hlink'>Diary</a></li>
+        <li><a href="/flowers"><img class='navimg' src="/public/images/flower-24.png" alt=""></a><a href="/flowers" class='Fnav-link hlink'>Flower</a></li>
+        <!-- <li><div id='search'>検索</div></li> -->
 
-    </ul>
+      </ul>
     </div>
-</div>
-<!-- フッターここまで ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝-->
+  </div>
+  <!-- フッターここまで ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝-->
 
 
 
-<script>
- const search = document.querySelector('#search')
- const hideSearch = document.querySelector('.t-search')
-   search.onclick = function(){
-     hideSearch.classList.toggle('t-search')
-    // alert('hit')
- }
-
-</script> 
+  <script>
+    const search = document.querySelector('#search')
+    const hideSearch = document.querySelector('.t-search')
+    search.onclick = function() {
+      hideSearch.classList.toggle('t-search')
+      // alert('hit')
+    }
+  </script>
 </body>
+
 </html>
--- src/View/myprofile.php
+++ src/View/myprofile.php
@@ -1,10 +1,39 @@
-<?php 
+<?php
 session_start();
-include('../../common/favicon.html') 
+include('../../common/funcs.php');
+$uid = $_SESSION['uid'];
+
+
+
+
+//DB接続
+$pdo = dbcon();
+
+//データ登録SQL作成
+$stmt = $pdo->prepare("SELECT * FROM shop where user_id=:uid");
+$stmt->bindValue('uid', $uid, PDO::PARAM_INT);
+$status = $stmt->execute();
+$result = $stmt->fetchAll();
+
+
+//データ登録処理後
+if ($status == false) {
+  $error = $stmt->errorInfo();
+  exit("SQLError:" . $error[2]);
+} else {
+
+  header('Location: /src/View/myprofile.php'); //Location:の後ろの半角スペースは必ず入れる。  
+}
+
+
+
 ?>
-    <title>登録情報</title>
-    <?php include('../../common/style.html') ?>
-    <link rel="stylesheet" href="/public/css/myprofile.css">
+
+
+<?php include('../../common/favicon.html'); ?>
+<title>登録情報</title>
+<?php include('../../common/style.html') ?>
+<link rel="stylesheet" href="/public/css/myprofile.css">
 
 </head>
 
@@ -13,13 +42,13 @@ include('../../common/favicon.html')
   <div class="grid-box">
     <header>
       <ul>
-        
+
         <div class='leftNav'>
-            <li><a href="/" ><img src="images/lf-logo-gray.png" alt="" class='logo' ></a></li>
-      </div>
-      <?php include('../../common/header-nav-leftIcon.html') ?>
+          <li><a href="/"><img src="images/lf-logo-gray.png" alt="" class='logo'></a></li>
+        </div>
+        <?php include('../../common/header-nav-leftIcon.html') ?>
         <div class='nav-right'>
-<!--         
+          <!--         
         <% if (typeof user == 'undefined') { %>
         <li class='log'><a href="/login" class='hlink'>Login</a></li>
         <% } else{%>
@@ -36,100 +65,99 @@ include('../../common/favicon.html')
                 <% } %>
           </a>
       </li> -->
-   </div>
+        </div>
 
       </ul>
-   </header>
+    </header>
 
-  
-    
+
+
 
     <div class="main">
-      <!-- <% items.forEach((item) => { %>
+      <?php foreach ($result as $item) : ?>
 
         <div class='pimg'>
-          <% if(item.account_img=== null){%>
-         <div class='pbox1'><img src="images/account3.png" alt=""></div>
-         <% }else{ %>
-          <div class='pbox1'><img src="<%= item.account_img %>" alt=""></div>
-          <% } %>
+          <?php if ([$item]['account_img']) : ?>
+            <!-- <% if(item.account_img=== null){%> -->
+            <div class='pbox1'><img src="images/account3.png" alt=""></div>
+          <?php else : ?>
+            <div class='pbox1'><img src="<?= [$item]['account_img']; ?>" alt=""></div>
+            <!-- <div class='pbox1'><img src="<%= item.account_img %>" alt=""></div> -->
+          <?php endif; ?>
           <div class="pbox2">
-            <div class='shopname'><%= item.name %></div>
-            <div class='shoptitle'><%= item.title %></div>
-            <div class='username'>アカウント名：<%= item.account_name %></div> 
-            <div class='link-text'>メール：<a class='link' href="<%= item.email %>"><%= item.email %></a></div>
-            <div class='link-text'>電話番号：<a class='link' href="tel:<%= item.tell %> "><%= item.tell %></a></div>
-            <div class='link-text'>HP：<a href="<%= item.web %>" class='link' target=”_blank”><%= item.web %></a></div>
+            <div class='shopname'><?= [$item]['name']; ?></div>
+            <div class='shoptitle'><?= [$item]['title']; ?></div>
+            <div class='username'>アカウント名：<?= [$item]['account_name']; ?></div>
+            <div class='link-text'>メール：<a class='link' href="<?= [$item]['email']; ?>"><?= [$item]['email']; ?></a></div>
+            <div class='link-text'>電話番号：<a class='link' href="tel:<?= [$item]['tell']; ?> "><?= [$item]['tell']; ?></a></div>
+            <div class='link-text'>HP：<a href="<?= [$item]['web']; ?>" class='link' target=”_blank”><?= [$item]['web']; ?></a></div>
 
+          </div>
         </div>
-      </div>
     </div>
-    <% }) %>
     <div class="line"></div>
-    <% items.forEach((item) => { %>
-     <div class="main2">
-        <h3>営業時間：<%= item.open %>　〜　<%= item.close %></h3>
-        <h3>定休日：<%= item.holiday %></h3>
-        <h3>住所：<%= item.location %></h3>
-        <div class="imgs">
-          <div class="img1">
-            <p>メイン画像</p>
-            <img src="<%= item.shop_img %>" alt="">
-          </div>
-          <div class="img1">
-            <p>店舗写真①</p>
-            <img src="<%= item.img1 %>" alt="">
-          </div>
-          <div class="img1">
-            <p>店舗写真②</p>
-            <img src="<%= item.img2 %>" alt="">
-          </div>
-    
+    <div class="main2">
+      <h3>営業時間：<?= [$item]['open']; ?>　〜　<?= [$item]['close']; ?></h3>
+      <h3>定休日：<?= [$item]['holiday']; ?></h3>
+      <h3>住所：<?= [$item]['location']; ?></h3>
+      <div class="imgs">
+        <div class="img1">
+          <p>メイン画像</p>
+          <img src="<?= [$item]['shop_img']; ?>" alt="">
         </div>
-        <h3>店舗タイトル：<br>&nbsp;<%= item.message %></h3>
-        <h3>店舗コメント</h3>
-        <p><%= item.comment %></p>
-        <h3>特徴：<%= item.feature %></h3>
-        <h3>【地図】</h3>
-        <iframe src="<%=item.map %>" width='100%' frameborder="0"></iframe>
-        <% }) %>
+        <div class="img1">
+          <p>店舗写真①</p>
+          <img src="<?= [$item]['img1']; ?>" alt="">
+        </div>
+        <div class="img1">
+          <p>店舗写真②</p>
+          <img src="<?= [$item]['img2']; ?>" alt="">
+        </div>
+
+      </div>
+      <h3>店舗タイトル：<br>&nbsp;<?= [$item]['message']; ?></h3>
+      <h3>店舗コメント</h3>
+      <p><?= [$item]['comment']; ?></p>
+      <h3>特徴：<?= [$item]['feature']; ?></h3>
+      <h3>【地図】</h3>
+      <iframe src="<?= [$item]['map']; ?>" width='100%' frameborder="0"></iframe>
 
     </div>
 
-        <div class="nav">
-          <% items.forEach((item) => { %>
-            <p><a href="/myprofileEdit/<%=item.id%>">店舗情報編集</a></p>
-          <% }) %> -->
-          <p><a href="drege.php">日記の登録</a></p>
-          <p><a href="frege.php">花の登録</a></p>
-          <p><a href="mapinfo.php">マップ情報</a></p>
-        </div>
+    <div class="nav">
+      <p><a href="/myprofileEdit/<%=item.id%>">店舗情報編集</a></p>
+    <?php endforeach; ?>
+    <p><a href="drege.php">日記の登録</a></p>
+    <p><a href="frege.php">花の登録</a></p>
+    <p><a href="mapinfo.php">マップ情報</a></p>
+    </div>
 
- </div>
-
-<!-- フッター ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝-->
-<div class="footer-glid">
-  <footer>
-      <h3>Copyright  second-cube</h3>
-  </footer>
-
-  <!-- フッターナビ -->
-  <div class='Fnav web ' >
-  <ul class='navs'>
-  <li ><a href="/map" ><img class='navimg' src="/public/images/map-24.png" alt=""></a><a href="/map" class='Fnav-link hlink'>Map</a></li>
-  <li ><a href="/shops" ><img class='navimg' src="/public/images/shop-24.png" alt=""></a><a href="/shops" class='Fnav-link hlink'>Shop</a></li>
-  <li ><a href="/diarys" ><img class='navimg' src="/public/images/script-24.png" alt=""></a><a href="/diarys" class='Fnav-link hlink'>Diary</a></li>
-  <li ><a href="/flowers" ><img class='navimg' src="/public/images/flower-24.png" alt=""></a><a href="/flowers" class='Fnav-link hlink'>Flower</a></li>
-  <!-- <li><div id='search'>検索</div></li> -->
-
-  </ul>
   </div>
-</div>
-<!-- フッターここまで ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝-->
+
+  <!-- フッター ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝-->
+  <div class="footer-glid">
+    <footer>
+      <h3>Copyright second-cube</h3>
+    </footer>
+
+    <!-- フッターナビ -->
+    <div class='Fnav web '>
+      <ul class='navs'>
+        <li><a href="/map"><img class='navimg' src="/public/images/map-24.png" alt=""></a><a href="/map" class='Fnav-link hlink'>Map</a></li>
+        <li><a href="/shops"><img class='navimg' src="/public/images/shop-24.png" alt=""></a><a href="/shops" class='Fnav-link hlink'>Shop</a></li>
+        <li><a href="/diarys"><img class='navimg' src="/public/images/script-24.png" alt=""></a><a href="/diarys" class='Fnav-link hlink'>Diary</a></li>
+        <li><a href="/flowers"><img class='navimg' src="/public/images/flower-24.png" alt=""></a><a href="/flowers" class='Fnav-link hlink'>Flower</a></li>
+        <!-- <li><div id='search'>検索</div></li> -->
+
+      </ul>
+    </div>
+  </div>
+  <!-- フッターここまで ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝-->
 
 
 
 
   </div>
 </body>
+
 </html>
