62a4b58366a18d4b392ea92de37d264812f5eed5
toshihisa handa <toshihisa.handa@caters.co.jp>
2021-02-12 13:04:00 +0900

    mapの登録ができていない

--- src/View/registerMap.php
+++ src/View/registerMap.php
@@ -1,7 +1,73 @@
 <?php 
 session_start();
-include('../../common/favicon.html') 
+include('../../common/funcs.php');
+$uid = $_SESSION['uid'];
+$uname = $_SESSION['uname'] ;
+$lat = $_POST['lat'];
+$lon = $_POST['lon'];
+// $pincolor = $_POST['pincolor'];
+$maptitle = $_POST['maptitle'];
+$description = $_POST['description'];
+
+
+
+if($_POST){
+
+//db接続
+dbcon();
+
+//sql作成
+$stmt = $pdo->prepare("INSERT INTO map(lat,lon,maptitle,description,created_at,user_id)VALUES(:lat,:lon,:maptitle,:description,sysdate(),user_id)");
+// $sql = "INSERT INTO map(lat,lon,maptitle,description,created_at,user_id)VALUES(:lat,:lon,:maptitle,:description,sysdate(),:user_id)";
+
+$stmt->bindValue(':lat', $lat, PDO::PARAM_INT);
+$stmt->bindValue(':lon', $lon, PDO::PARAM_INT);
+$stmt->bindValue(':maptitle', $maptitle, PDO::PARAM_INT);
+$stmt->bindValue(':description', $description, PDO::PARAM_INT);
+$stmt->bindValue(':user_id', $uid, PDO::PARAM_INT);
+
+// $stmt->bindValue(':lat',$lat, PDO::PARAM_INT);
+// $stmt->bindValue(':lon',$lon, PDO::PARAM_INT);
+// $stmt->bindValue(':maptitle',$maptitle, PDO::PARAM_STR);
+// $stmt->bindValue(':description',$description, PDO::PARAM_STR);
+// $stmt->bindValue(':user_id',$uid, PDO::PARAM_INT);
+// $stmt->bindValue(':pincolor',$pincolor, PDO::PARAM_STR);
+
+$status = $stmt->execute();
+
+echo 'hoge';
+return;
+
+//４．データ登録処理後
+if ($status == false) {
+  $error = $stmt->errorInfo();
+  exit("SQLError:" . $error[2]);
+} else {
+  
+  header('Location: /index.php'); //Location:の後ろの半角スペースは必ず入れる。
+  exit();
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
+
+
+
+<?php include('../../common/favicon.html') ?>
     <title>マップ情報編集</title>
     <?php include('../../common/style.html') ?>
     <link rel="stylesheet" href="/public/css/mapEdit.css">
@@ -48,22 +114,22 @@ include('../../common/favicon.html')
               </div><br>
             <button type="submit" class='sends'>送信</button>
         </form>
-        <form action='/mapUpdate/<%=item.id%>' method="post" >
-            <!-- <div class='inframe'>
-              <div>　　緯度</div><input class='inputs' type="text" name="lat" value='<%= item.lat%>'><br>
+        <form  method="post" >
+            <div class='inframe'>
+              <div>　　緯度</div><input class='inputs' type="text" name="lat" value=''><br>
           </div>
             <div class='inframe'>
-              <div>　　経度</div><input class='inputs' type="text" name="lon" value='<%= item.lon%>'><br>
+              <div>　　経度</div><input class='inputs' type="text" name="lon" value=''><br>
           </div>
-            <div class='inframe maphidden'>
+            <!-- <div class='inframe maphidden'>
               <div>ピンの色</div><input class='inputs' type="text" name="pincolor" value='<%= item.pincolor%>'><br>
-          </div>
+          </div> -->
             <div class='inframe '>
-              <div>タイトル</div><input class='inputs' type="text" name="maptitle" value='<%= item.title%>'><br>
+              <div>タイトル</div><input class='inputs' type="text" name="maptitle" value='<?=$uname?>'><br>
           </div>
             <div class='inframe'>
-              <div>　　説明</div><textarea class='txt'  name="description" ><%= item.description%></textarea><br>
-          </div> -->
+              <div>　　説明</div><textarea class='txt'  name="description" ></textarea><br>
+          </div>
        
           <button type="submit" class='sends'>送信</button>
       </form>
