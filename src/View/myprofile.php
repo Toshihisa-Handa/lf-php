f4b12f40688abe3ab39fc17f11a1638918c39ba3
toshihisa handa <toshihisa.handa@caters.co.jp>
2021-02-12 18:13:36 +0900

    myprofielのテキストの方更新実装。画像はまだ

--- src/View/myprofile.php
+++ src/View/myprofile.php
@@ -9,6 +9,7 @@ $uid = $_SESSION['uid'];
 //DB接続
 $pdo = dbcon();
 
+
 //データ登録SQL作成
 $stmt = $pdo->prepare("SELECT * FROM shop where user_id=:uid");
 $stmt->bindValue('uid', $uid, PDO::PARAM_INT);
@@ -16,15 +17,12 @@ $status = $stmt->execute();
 $result = $stmt->fetchAll();
 
 
+
 //データ登録処理後
 if ($status == false) {
   $error = $stmt->errorInfo();
   exit("SQLError:" . $error[2]);
-} else {
-
-  header('Location: /src/View/myprofile.php'); //Location:の後ろの半角スペースは必ず入れる。  
-}
-
+} 
 
 
 ?>
@@ -77,55 +75,55 @@ if ($status == false) {
       <?php foreach ($result as $item) : ?>
 
         <div class='pimg'>
-          <?php if ([$item]['account_img']) : ?>
+          <?php if ($item['account_img']) : ?>
             <!-- <% if(item.account_img=== null){%> -->
             <div class='pbox1'><img src="images/account3.png" alt=""></div>
           <?php else : ?>
-            <div class='pbox1'><img src="<?= [$item]['account_img']; ?>" alt=""></div>
+            <div class='pbox1'><img src="<?= $item['account_img']; ?>" alt=""></div>
             <!-- <div class='pbox1'><img src="<%= item.account_img %>" alt=""></div> -->
           <?php endif; ?>
           <div class="pbox2">
-            <div class='shopname'><?= [$item]['name']; ?></div>
-            <div class='shoptitle'><?= [$item]['title']; ?></div>
-            <div class='username'>アカウント名：<?= [$item]['account_name']; ?></div>
-            <div class='link-text'>メール：<a class='link' href="<?= [$item]['email']; ?>"><?= [$item]['email']; ?></a></div>
-            <div class='link-text'>電話番号：<a class='link' href="tel:<?= [$item]['tell']; ?> "><?= [$item]['tell']; ?></a></div>
-            <div class='link-text'>HP：<a href="<?= [$item]['web']; ?>" class='link' target=”_blank”><?= [$item]['web']; ?></a></div>
+            <div class='shopname'><?= $item['name']; ?></div>
+            <div class='shoptitle'><?= $item['title']; ?></div>
+            <div class='username'>アカウント名：<?= $item['account_name']; ?></div>
+            <div class='link-text'>メール：<a class='link' href="<?= $item['email']; ?>"><?= $item['email']; ?></a></div>
+            <div class='link-text'>電話番号：<a class='link' href="tel:<?= $item['tell']; ?> "><?= $item['tell']; ?></a></div>
+            <div class='link-text'>HP：<a href="<?= $item['web']; ?>" class='link' target=”_blank”><?= $item['web']; ?></a></div>
 
           </div>
         </div>
     </div>
     <div class="line"></div>
     <div class="main2">
-      <h3>営業時間：<?= [$item]['open']; ?>　〜　<?= [$item]['close']; ?></h3>
-      <h3>定休日：<?= [$item]['holiday']; ?></h3>
-      <h3>住所：<?= [$item]['location']; ?></h3>
+      <h3>営業時間：<?= $item['open']; ?>　〜　<?= $item['close']; ?></h3>
+      <h3>定休日：<?= $item['holiday']; ?></h3>
+      <h3>住所：<?= $item['location']; ?></h3>
       <div class="imgs">
         <div class="img1">
           <p>メイン画像</p>
-          <img src="<?= [$item]['shop_img']; ?>" alt="">
+          <img src="<?= $item['shop_img']; ?>" alt="">
         </div>
         <div class="img1">
           <p>店舗写真①</p>
-          <img src="<?= [$item]['img1']; ?>" alt="">
+          <img src="<?= $item['img1']; ?>" alt="">
         </div>
         <div class="img1">
           <p>店舗写真②</p>
-          <img src="<?= [$item]['img2']; ?>" alt="">
+          <img src="<?= $item['img2']; ?>" alt="">
         </div>
 
       </div>
-      <h3>店舗タイトル：<br>&nbsp;<?= [$item]['message']; ?></h3>
+      <h3>店舗タイトル：<br>&nbsp;<?= $item['message']; ?></h3>
       <h3>店舗コメント</h3>
-      <p><?= [$item]['comment']; ?></p>
-      <h3>特徴：<?= [$item]['feature']; ?></h3>
+      <p><?= $item['comment']; ?></p>
+      <h3>特徴：<?= $item['feature']; ?></h3>
       <h3>【地図】</h3>
-      <iframe src="<?= [$item]['map']; ?>" width='100%' frameborder="0"></iframe>
+      <iframe src="<?= $item['map']; ?>" width='100%' frameborder="0"></iframe>
 
     </div>
 
     <div class="nav">
-      <p><a href="/myprofileEdit/<%=item.id%>">店舗情報編集</a></p>
+      <p><a href="myprofileEdit.php/<?= $item['uid']; ?>">店舗情報編集</a></p>
     <?php endforeach; ?>
     <p><a href="drege.php">日記の登録</a></p>
     <p><a href="frege.php">花の登録</a></p>
---
+++ src/View/myprofileEditUpdate.php
@@ -0,0 +1,58 @@
+<?php
+session_start();
+include('../../common/funcs.php');
+
+
+//1. POSTデータ取得
+$id = $_POST['id'];
+$name = $_POST['name'];
+$title = $_POST['title'];
+$account_name = $_POST['account_name'];
+$web = $_POST['web'];
+$email = $_POST['email'];
+$tell = $_POST['tell'];
+$open = $_POST['open'];
+$close = $_POST['close'];
+$holiday = $_POST['holiday'];
+$location = $_POST['location'];
+$map = $_POST['map'];
+$message = $_POST['message'];
+$comment = $_POST['comment'];
+$feature = $_POST['feature'];
+
+//DB接続
+$pdo = dbcon();
+
+
+//データ登録SQL作成
+$sql = 'UPDATE shop SET name=:name,title=:title,account_name=:account_name,web=:web,
+email=:email,tell=:tell,open=:open,close=:close,holiday=:holiday,location=:location,map=:map,
+message=:message,comment=:comment,feature=:feature WHERE id=:id';
+
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(':name', $name, PDO::PARAM_STR);
+$stmt->bindValue(':title', $title, PDO::PARAM_STR);
+$stmt->bindValue(':account_name', $account_name, PDO::PARAM_STR);
+$stmt->bindValue(':web', $web, PDO::PARAM_STR);
+$stmt->bindValue(':email', $email, PDO::PARAM_STR);
+$stmt->bindValue(':tell', $tell, PDO::PARAM_INT);
+$stmt->bindValue(':open', $open, PDO::PARAM_STR);
+$stmt->bindValue(':close', $close, PDO::PARAM_STR);
+$stmt->bindValue(':holiday', $holiday, PDO::PARAM_STR);
+$stmt->bindValue(':location', $location, PDO::PARAM_STR);
+$stmt->bindValue(':map', $map, PDO::PARAM_STR);
+$stmt->bindValue(':message', $message, PDO::PARAM_STR);
+$stmt->bindValue(':comment', $comment, PDO::PARAM_STR);
+$stmt->bindValue(':feature', $feature, PDO::PARAM_STR);
+$stmt->bindValue(':id',   $id,     PDO::PARAM_INT);  
+$status = $stmt->execute();
+
+
+//データ登録処理後
+if ($status == false) {
+  $error = $stmt->errorInfo();
+  exit("SQLError:" . $error[2]);
+} else {
+  header('Location: /src/View/myprofileEdit.php');
+  exit;
+}
